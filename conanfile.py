76a34d296457a3852946580d9aa9931ca99780ca
pwjworks <13610513466@163.com>
2024-05-09 22:16:56 +0800

    update conan

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 from conan.tools.gnu import AutotoolsToolchain, Autotools
-from conan.tools.layout import basic_layout
-from conan.tools.apple import fix_apple_shared_install_name 
+from conan.tools.apple import fix_apple_shared_install_name
+
 
 class ffmpegRecipe(ConanFile):
     name = "ffmpeg"
@@ -25,22 +25,37 @@ class ffmpegRecipe(ConanFile):
     def generate(self):
         at_toolchain = AutotoolsToolchain(self)
         tools_path = "/home/pwjworks/android-ndk-r26d-linux/android-ndk-r26d/toolchains/llvm/prebuilt/linux-x86_64/bin"
-        cc = tools_path + "/aarch64-linux-android%s-clang" % str(self.settings.os.api_level)
-        cxx = tools_path + "/aarch64-linux-android%s-clang++" % str(self.settings.os.api_level)
+        cc = tools_path + "/aarch64-linux-android%s-clang" % str(
+            self.settings.os.api_level
+        )
+        cxx = tools_path + "/aarch64-linux-android%s-clang++" % str(
+            self.settings.os.api_level
+        )
         nm = tools_path + "/llvm-nm"
         llvm_ar = tools_path + "/llvm-ar"
-        at_toolchain.configure_args.extend(["--enable-small", "--disable-programs", "--disable-avdevice",
-                                       "--disable-doc", "--disable-symver",  "--enable-gpl",
-                                       "--enable-pic", "--disable-debug",
-                                       "--disable-postproc", "--enable-neon", "--enable-jni",
-                                       "--disable-x86asm", "--enable-cross-compile", "--disable-stripping",
-                                       "--cpu=armv8-a",
-                                       "--cc=%s" % cc,
-                                       "--cxx=%s" % cxx,
-                                       "--nm=%s" % nm,
-                                       "--ar=%s" % llvm_ar,
-                                       "--target-os=android",
-                                       ])
+        at_toolchain.configure_args.extend(
+            [
+                "--enable-small",
+                "--enable-gpl",
+                "--enable-pic",
+                "--enable-neon",
+                "--enable-jni",
+                "--enable-cross-compile",
+                "--enable-hardcoded-tables",
+                "--disable-programs",
+                "--disable-avdevice",
+                "--disable-doc",
+                "--disable-symver",
+                "--disable-postproc",
+                "--disable-x86asm",
+                "--disable-stripping",
+                "--cc=%s" % cc,
+                "--cxx=%s" % cxx,
+                "--nm=%s" % nm,
+                "--ar=%s" % llvm_ar,
+                "--target-os=android",
+            ]
+        )
         at_toolchain.configure_args.remove("--sbindir=${prefix}/bin")
         at_toolchain.configure_args.remove("--includedir=${prefix}/include")
         at_toolchain.configure_args.remove("--oldincludedir=${prefix}/include")
@@ -48,15 +63,20 @@ class ffmpegRecipe(ConanFile):
         at_toolchain.configure_args.remove("--build=x86_64-linux-gnu")
         if self.settings.arch == "x86":
             at_toolchain.configure_args.append("--cpu=x86")
+            at_toolchain.configure_args.append("--arch=x86")
         elif self.settings.arch == "x86_64":
             at_toolchain.configure_args.append("--cpu=x86_64")
+            at_toolchain.configure_args.append("--arch=x86_64")
         elif self.settings.arch == "armv8":
             at_toolchain.configure_args.append("--cpu=armv8-a")
+            at_toolchain.configure_args.append("--arch=aarch64")
         elif self.settings.arch == "armv7":
             at_toolchain.configure_args.append("--cpu=armv7-a")
-            
+            at_toolchain.configure_args.append("--arch=arm")
+
         if self.settings.build_type == "Debug":
-            at_toolchain.configure_args.append("--enable-debug")
+            at_toolchain.configure_args.append("--enable-debug=3")
+            at_toolchain.configure_args.append("--disable-stripping")
         elif self.settings.build_type == "Release":
             at_toolchain.configure_args.append("--disable-debug")
         at_toolchain.generate()
